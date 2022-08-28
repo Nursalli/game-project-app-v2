ec4320bad715d9a2cc33a80ab305847664010c3d
G. Ferry <grgsferry@gmail.com>
2022-08-28 20:25:04 +0700

    slicing game listing

--- pages/games/index.js
+++ pages/games/index.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import Navbar from '../../components/Navbar';
+import Footer from '../../components/Footer';
+import GameCard from '../../components/GameCard';
 
 function Index() {
   return (
-    <div>Index</div>
+    <>
+      <Navbar />
+      <div className="text-3xl font-bold text-center mb-5 mt-8">List of Our Games</div>
+      <div className="md:grid md:grid-cols-3 md:gap-8 px-10 pt-4 pb-16 xl:px-40 2xl:px-80">
+        <GameCard />
+        <GameCard />
+        <GameCard />
+        <GameCard />
+        <GameCard />
+        <GameCard />
+        <GameCard />
+        <GameCard />
+        <GameCard />
+      </div>
+      <Footer />
+    </>
   )
 }
 
