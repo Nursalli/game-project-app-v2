cbaeb78d25f20180f10d43addbbdbeb66599295f
G. Ferry <73511271+grgsferry@users.noreply.github.com>
2022-08-28 22:21:28 +0700

    [FE][Slicing] Slicing Game List #5

--- components/SeeMore.js
+++ components/SeeMore.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function GameCard() {
     return (
         <div className="text-center mb-4">
-            <Link href="/">
+            <Link href="/games">
                 <button className="bg-white hover:bg-[#247BA0] border-solid border-[#247BA0] border-[1px] font-bold text-xl text-[#247BA0] hover:text-white rounded-lg h-12 w-64">
                     More Games
                 </button>
