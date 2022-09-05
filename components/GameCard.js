b15e5e9d8e8cbe6eca8a36dcc83768639497b205
enggarlr <96070230+enggarlr@users.noreply.github.com>
2022-09-05 22:37:58 +0700

    [FE][Integration] Integration Game List (#19)
    * [FE][Integration] Integration Game List
    
    * Bug fixing/Uncompleted variable declaration

--- components/GameCard.js
+++ components/GameCard.js
@@ -1,26 +1,26 @@
 import React from 'react'
 import Link from 'next/link'
 
-export default function GameCard() {
+export default function GameCard(props) {    
   return (
     <div className="bg-white border-solid border-[1px] border-[#D7D7D7] rounded-lg w-full h-full">
-        <div className="w-full h-60 bg-no-repeat bg-center bg-cover rounded-t-lg" style={{backgroundImage: "url(/img/werewolf.jpg)"}}></div>
+        <div className="w-full h-60 bg-no-repeat bg-center bg-cover rounded-t-lg" style={{backgroundImage: `url(${props.thumbnails || "/img/werewolf.jpg"})`}}></div>
         <div className="mx-4 my-4">
-            <div className="font-bold text-xl mb-2">Rock-Paper-Scissors Random</div>
-            <div>Play Rock-Paper-Scissors with Computer (Organic Random)</div>
+            <div className="font-bold text-xl mb-2">{props.title}</div>
+            <div>{props.description}</div>
             <div className="columns-2 my-4">
                 <div className="flex flex-row justify-center">
                     <div>👁️</div>
-                    <div>5 users</div>
+                    <div>{props.viewCount} users</div>
                 </div>
                 <div className="flex flex-row justify-center">
                     <div>🎮</div>
-                    <div>3 users</div>
+                    <div>{props.playCount} users</div>
                 </div>
             </div>
             <div>
                 <Link href="/">
-                    <button className="bg-white hover:bg-[#70C1B3] text-[#70C1B3] hover:text-white font-semibold hover:font-bold border-solid border-[1px] border-[#70C1B3] rounded-lg w-full h-10">
+                    <button className="bg-white hover:bg-[#70C1B3] text-[#70C1B3] hover:text-white font-semibold hover:font-bold border-solid border-[1px] border-[#70C1B3] rounded-lg w-full h-10" onClick={props.handlePlay}>
                         Play This Game
                     </button>
                 </Link>
