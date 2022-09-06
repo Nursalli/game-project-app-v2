5b89ef179c474a3da747dd540cecfcdd847f2648
G. Ferry <73511271+grgsferry@users.noreply.github.com>
2022-09-06 21:50:04 +0700

    [FE][Integration] Playing Suit Game (#21)
    * first commit setting redux state
    
    * second commit pushing round to runningrounds
    
    * third commit add logic suit and more state logic
    
    * fourth commit bug fix and post game data
    
    * fifth commit add integration to play/com

--- reducer/game.reducer.js
+++ reducer/game.reducer.js
@@ -2,13 +2,16 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const gameSlice = createSlice({
   name: "game",
-  initialState: { runningGame: [] },
+  initialState: { runningGame: "", initGame: "", finalResult: "" },
   reducers: {
     setRunningGame: (state, { payload }) => {
       state.runningGame = payload;
     },
+    setInitGame: (state, { payload }) => {
+      state.initGame = payload;
+    }
   },
 });
 
-export const { setRunningGame } = gameSlice.actions;
+export const { setRunningGame, setInitGame } = gameSlice.actions;
 export default gameSlice.reducer;
