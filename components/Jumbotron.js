04c3fccc594aadcb0009ad4158f6286969bfde7a
corwin.xyz <corwin.xyz@gmail.com>
2022-09-07 20:38:49 +0700

    revisi integration landing

--- components/Jumbotron.js
+++ components/Jumbotron.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import Image from 'next/image'
 
 export default function Jumbotron() {
@@ -10,13 +10,13 @@ export default function Jumbotron() {
                 <div className="font-black text-4xl text-[#247BA0]">GAME PROJECT</div>
                 <div>Welcome! Here you can play many kind of games created by people around the world. Support us by clicking share buttons below!</div>
                 <div className="mt-3 flex flex-row justify-center">
-                    <button className="bg-slate-100 hover:bg-[#70C1B3] h-10 w-10 rounded-lg mx-1 flex items-center justify-center bg-center bg-no-repeat bg-[length:25px_25px]" style={{backgroundImage: "url(/img/facebook-logo.svg)"}}>
+                    <button className="bg-slate-100 hover:bg-[#70C1B3] h-10 w-10 rounded-lg mx-1 flex items-center justify-center bg-center bg-no-repeat bg-[length:25px_25px]" style={{backgroundImage: "url(/img/facebook-logo.svg)"}} href="https://www.facebook.com/binaracademy/">
                     </button>
-                    <button className="bg-slate-100 hover:bg-[#70C1B3] h-10 w-10 rounded-lg mx-1 flex items-center justify-center bg-center bg-no-repeat bg-[length:25px_25px]" style={{backgroundImage: "url(/img/instagram-logo.svg)"}}>
+                    <button className="bg-slate-100 hover:bg-[#70C1B3] h-10 w-10 rounded-lg mx-1 flex items-center justify-center bg-center bg-no-repeat bg-[length:25px_25px]" style={{backgroundImage: "url(/img/instagram-logo.svg)"}} href="https://www.instagram.com/academybinar/">
                     </button>
-                    <button className="bg-slate-100 hover:bg-[#70C1B3] h-10 w-10 rounded-lg mx-1 flex items-center justify-center bg-center bg-no-repeat bg-[length:25px_25px]" style={{backgroundImage: "url(/img/twitter-logo.svg)"}}>
+                    <button className="bg-slate-100 hover:bg-[#70C1B3] h-10 w-10 rounded-lg mx-1 flex items-center justify-center bg-center bg-no-repeat bg-[length:25px_25px]" style={{backgroundImage: "url(/img/twitter-logo.svg)"}} href="https://twitter.com/academybinar">
                     </button>
-                    <button className="bg-slate-100 hover:bg-[#70C1B3] h-10 w-10 rounded-lg mx-1 flex items-center justify-center bg-center bg-no-repeat bg-[length:25px_25px]" style={{backgroundImage: "url(/img/gmail-logo.svg)"}}>
+                    <button className="bg-slate-100 hover:bg-[#70C1B3] h-10 w-10 rounded-lg mx-1 flex items-center justify-center bg-center bg-no-repeat bg-[length:25px_25px]" style={{backgroundImage: "url(/img/gmail-logo.svg)"}} href="https://www.youtube.com/c/BinarAcademy">
                     </button>
                 </div>
             </div>
--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,47 @@ import Footer from '../components/Footer';
 import Jumbotron from '../components/Jumbotron';
 import GameCard from '../components/GameCard';
 import SeeMore from '../components/SeeMore';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 export default function Home() {
+
+  const [games, setGame] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(process.env.NEXT_PUBLIC_BASE_URL + "games/landing")
+      .then((res) => {
+        setGame(res.data?.data);
+        })
+      .catch((err) => {
+      console.log(err);
+      });
+  })
+
   return (
     <>
       <Navbar />
       <Jumbotron />
       <div className="text-3xl font-bold text-center mb-5 mt-8">Most Played Games</div>
       <div className="md:grid md:grid-cols-3 md:gap-8 px-10 py-4 xl:px-40 2xl:px-80">
-        <GameCard />
-        <GameCard />
-        <GameCard />
+        {
+          games.map((game) => {
+            return(
+              <div key={game.id}>
+                <GameCard
+                  id = {game.id} 
+                  thumbnails= {game.thumbnail}
+                  title= {game.title}
+                  description= {game.description}
+                  viewCount= {game.viewCount}
+                  playCount= {game.playCount}
+                 
+                />
+              </div>
+            )
+          })
+        }  
       </div>
       <SeeMore />
       <Footer />
