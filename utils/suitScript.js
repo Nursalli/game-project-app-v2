b47108302d44705b91d1237257241201cf0bdd6a
GDP-IrvanAriyanto <irvan.ariyanto@gdplabs.id>
2022-09-09 07:49:02 +0700

    fix error calculation

--- utils/suitScript.js
+++ utils/suitScript.js
@@ -1,36 +1,39 @@
 const calculateSuit = (userInput, computerInput) => {
-    if (userInput === computerInput) {
-        return "draw"
-    } else if (userInput === "rock" && computerInput === "scissors") {
-        return "win"
-    } else if (userInput === "paper" && computerInput === "rock") {
-        return "win"
-    } else if (userInput === "scissors" && computerInput === "paper") {
-        return "win"
-    } else {
-        return "lose"
-    }
-}
+  if (userInput === computerInput) {
+    return 'draw';
+  } else if (userInput === 'rock' && computerInput === 'scissors') {
+    return 'win';
+  } else if (userInput === 'paper' && computerInput === 'rock') {
+    return 'win';
+  } else if (userInput === 'scissors' && computerInput === 'paper') {
+    return 'win';
+  } else {
+    return 'lose';
+  }
+};
 
 const resultConverter = (input) => {
-    if (input === "win") {
-        return 1
-    } else if (input === "lose") {
-        return -1
-    } else {
-        return 0
-    }
-}
+  if (input === 'win') {
+    return 1;
+  } else if (input === 'lose') {
+    return -1;
+  } else {
+    return 0;
+  }
+};
 
-const calculateRounds = (input1, input2, input3) => {
-    const totalRound = resultConverter(input1) + resultConverter(input2) + resultConverter(input3);
-    if (totalRound > 0) {
-        return "win"
-    } else if (totalRound < 0) {
-        return "lose"
-    } else {
-        return "draw"
-    }
-}
+const calculateRounds = (rounds) => {
+  const totalRound = 0;
+  rounds.map((round) => {
+    totalRound += resultConverter(round.result);
+  });
+  if (totalRound > 0) {
+    return 'win';
+  } else if (totalRound < 0) {
+    return 'lose';
+  } else {
+    return 'draw';
+  }
+};
 
 export { calculateSuit, calculateRounds };
